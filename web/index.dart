d474718b600549c4edbabaef43954c24c043af31
montyr75 <montyr75@gmail.com>
2015-11-19 13:47:55 -0700

    fix for iron_flex_layout import

--- web/index.dart
+++ web/index.dart
@@ -1,7 +1,7 @@
 library pdcl_maps.web.index;
 
 import 'package:pdcl_maps/main_app.dart';
-import 'package:polymer_elements/iron_flex_layout.dart';
+import 'package:polymer_elements/iron_flex_layout/classes/iron_flex_layout.dart';
 import 'package:polymer/polymer.dart';
 
 /// [MainApp] used!
